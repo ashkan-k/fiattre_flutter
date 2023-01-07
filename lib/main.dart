8ab64233f450260dc94e6fb2df4704da6ce5f969
ashkan <karimiashkan8181@gmail.com>
2023-01-08 00:29:25 +0330

    init Themes and base

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:fiatre_app/pages/layouts/base_page.dart';
+import 'package:fiatre_app/providers/MyThemeProvider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
-  runApp(const FiatreApp());
+  // Disable orientation
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]);
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => MyThemeProvider()),
+      ],
+      child: const FiatreApp(),
+    )
+  );
 }
 
 class FiatreApp extends StatelessWidget {
@@ -11,11 +29,27 @@ class FiatreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<MyThemeProvider>(
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'فیاتر',
-        home: BasePage(),
-      ),
+      builder: (context, my_theme_provider, child) {
+        return MaterialApp(
+          themeMode: my_theme_provider.themeMode,
+          darkTheme: MyThemes.darkTheme,
+          theme: MyThemes.lightTheme,
+          localizationsDelegates: const [
+            AppLocalizations.delegate, // Add this line
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en', ''), // English, no country code
+            Locale('fa', ''), // Persian, no country code
+          ],
+          locale: Locale('en', ''),
+          debugShowCheckedModeBanner: false,
+          title: 'فیاتر',
+          home: BasePage(),
+        );
+      },
     );
   }
 }
